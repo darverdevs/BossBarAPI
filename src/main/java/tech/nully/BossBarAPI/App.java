931ecfd8ab259664df730415ccb61f62f632374b
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-06-24 14:02:36 -0700

    set stuff up

---
+++ src/main/java/tech/nully/BossBarAPI/App.java
@@ -0,0 +1,13 @@
+package BossBarAPI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/BossBarAPI/AppTest.java
@@ -0,0 +1,20 @@
+package BossBarAPI;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
